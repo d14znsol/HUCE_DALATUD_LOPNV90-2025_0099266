4580e20bc4c64044aac0fc2278a5156fce5090f8
Th2SonTranThai0099266 <ekkoisthebest228@gmail.com>
2025-11-20 18:41:44 +0700

    0099266/MVVM/ Tạo file addin
    đã hoàn thành

---
+++ HUCE_DALATUD_LOPNV90_2025_0099266/Command/CmdProRename.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using HUCE_DALATUD_LOPNV90_2025_0099266.Views;
+
+namespace ProRename.Addin
+{
+    [Transaction(TransactionMode.Manual)]
+    public class CmdProRename : IExternalCommand
+    {
+        public Result Execute(
+            ExternalCommandData commandData,
+            ref string message,
+            ElementSet elements)
+        {
+            // Sau này bạn có thể lấy UIDocument, Document ở đây
+            // UIDocument uidoc = commandData.Application.ActiveUIDocument;
+            // Document doc = uidoc.Document;
+
+            // Gọi UI WPF có sẵn
+            var win = new MAINWindow();
+            win.ShowDialog();
+
+            return Result.Succeeded;
+        }
+    }
+}
