f0cc8e4181098b3095d5f07e2d0da22d825eea7f
bin02072001-boop <bin02072001@gmail.com>
2025-12-11 20:39:59 +0700

    0159467/MVVM/Laydulieu
    Lấy dữ liệu và thuật toán đổi tên các Level

--- HUCE_DALATUD_LOPNV90_2025_0099266/ViewModels/RenameLevelsViewModel.cs
+++ HUCE_DALATUD_LOPNV90_2025_0099266/ViewModels/RenameLevelsViewModel.cs
@@ -1,12 +1,155 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Autodesk.Revit.DB;
+using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace HUCE_DALATUD_LOPNV90_2025_0099266.ViewModels
 {
-    public class RenameLevelsViewModel
+    public class RenameLevelsViewModel : INotifyPropertyChanged
     {
+        private readonly Document _doc;
+
+        // Collection bind vào DataGrid trong ReNameLevels.xaml
+        public ObservableCollection<LevelsModels> ReLevels { get; }
+            = new ObservableCollection<LevelsModels>();
+
+        // --- FILTER PROPERTIES ---
+        private string _filterText;
+        public string FilterText
+        {
+            get => _filterText;
+            set { if (_filterText != value) { _filterText = value; OnPropertyChanged(nameof(FilterText)); } }
+        }
+
+        private string _filterCategory;
+        public string FilterCategory
+        {
+            get => _filterCategory;
+            set { if (_filterCategory != value) { _filterCategory = value; OnPropertyChanged(nameof(FilterCategory)); ApplyFilter(); } }
+        }
+
+        public ObservableCollection<string> Categories { get; } = new ObservableCollection<string>();
+
+        // --- COMMANDS ---
+        public ICommand FilterCommand { get; }
+        public ICommand ShowAllCommand { get; }
+        public ICommand CheckAllCommand { get; }
+        public ICommand UncheckAllCommand { get; }
+        public ICommand RenameCommand { get; }
+
+        // --- RENAME RULES (Copy giống hệt các ViewModel khác) ---
+        public int RemoveFirst { get; set; }
+        public int RemoveLast { get; set; }
+        public string AddPrefix { get; set; }
+        public string AddSuffix { get; set; }
+        public string FindText { get; set; }
+        public string ReplaceWith { get; set; }
+        public int StartIndex { get; set; }
+        public int RemoveCount { get; set; }
+        public string AddText { get; set; }
+        public bool ToUppercase { get; set; }
+        public bool Lowercase { get; set; }
+        public bool RemoveDiacritics { get; set; }
+        public bool ISO19650 { get; set; }
+
+        public RenameLevelsViewModel(Document doc)
+        {
+            _doc = doc;
+            LoadLevels();
+
+            FilterCommand = new RelayCommand(ApplyFilter);
+            ShowAllCommand = new RelayCommand(ShowAll);
+            CheckAllCommand = new RelayCommand(CheckAll);
+            UncheckAllCommand = new RelayCommand(UncheckAll);
+            RenameCommand = new RelayCommand(ExecuteRename, CanExecuteRename);
+
+            CollectCategories();
+        }
+
+        private void LoadLevels()
+        {
+            ReLevels.Clear();
+            // Lọc lấy tất cả đối tượng Level
+            var collector = new FilteredElementCollector(_doc)
+                .OfClass(typeof(Level))
+                .Cast<Level>();
+
+            foreach (var lv in collector)
+            {
+                ReLevels.Add(new LevelsModels(lv));
+            }
+        }
+        private void ExecuteRename()
+        {
+            using (var tx = new Transaction(_doc, "Rename Levels"))
+            {
+                tx.Start();
+                foreach (var item in ReLevels.Where(x => x.IsSelected))
+                {
+                    try
+                    {
+                        var lv = _doc.GetElement(item.LevelId) as Level;
+                        if (lv == null) continue;
+
+                        string newName = ComputeNewName(item.TypeName);
+
+                        // Đổi tên Level
+                        lv.Name = newName;
+
+                        // Update UI
+                        item.TypeName = newName;
+                        item.NewTypeName = newName;
+                    }
+                    catch (Exception)
+                    {
+                        // Level trùng tên sẽ gây lỗi, Revit tự báo hoặc ta bỏ qua
+                    }
+                }
+                tx.Commit();
+            }
+        }
+
+        // --- LOGIC XỬ LÝ CHUỖI & FILTER (Dùng chung cho cả dự án) ---
+        // Bạn copy hàm ComputeNewName, ApplyFilter, ShowAll... từ RenameFamiliesViewModel sang đây nhé
+        // Vì logic xử lý text là y hệt nhau.
+
+        // Dưới đây là ví dụ hàm quan trọng nhất cần có để code không lỗi:
+        private string ComputeNewName(string oldName)
+        {
+            string name = oldName;
+            // ... (Copy nội dung xử lý chuỗi: Substring, Replace, Insert...)
+            // Để code chạy được ngay, mình để tạm logic đơn giản, bạn nhớ paste code đầy đủ vào:
+            if (!string.IsNullOrEmpty(AddPrefix)) name = AddPrefix + name;
+            if (!string.IsNullOrEmpty(AddSuffix)) name = name + AddSuffix;
+            // ...
+            return name;
+        }
+
+        private void CollectCategories()
+        {
+            Categories.Clear();
+            Categories.Add("Levels"); // Level chỉ có 1 category duy nhất
+        }
+
+        private void ApplyFilter()
+        {
+            foreach (var item in ReLevels)
+            {
+                bool match = true;
+                if (!string.IsNullOrEmpty(FilterText) && item.TypeName.IndexOf(FilterText, StringComparison.OrdinalIgnoreCase) < 0)
+                    match = false;
+                item.IsSelected = match;
+            }
+        }
+
+        private void ShowAll() { foreach (var i in ReLevels) i.IsSelected = true; }
+        private void CheckAll() { foreach (var i in ReLevels) i.IsSelected = true; }
+        private void UncheckAll() { foreach (var i in ReLevels) i.IsSelected = false; }
+        private bool CanExecuteRename() { return ReLevels.Any(x => x.IsSelected); }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string name) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
     }
 }
